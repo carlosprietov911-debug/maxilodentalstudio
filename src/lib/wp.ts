38d595eb86ddec42211140ec966bb7e0d078a39d
Crlosprieto <carlosprieto316@gmail.com>
2026-03-29 21:37:37 -0500

    actualizo casosclinicos y wordpress

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -1,23 +1,7 @@
-const domain = import.meta.env.WP_DOMAIN
-const apiUrl = `${domain}/wp-json/wp/v2`
-
-export const getPageInfo = async (slug: string) => {
-  const response = await fetch(`${apiUrl}/pages?slug=${slug}`)
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch page info")
-  }
-
-  const [data] = await response.json()
-
-  if (!data) {
-    throw new Error(`No page found with slug: ${slug}`)
-  }
-
-  const {
-    title: { rendered: title },
-    content: { rendered: content },
-  } = data
-
-  return { title, content }
+// src/lib/wordpress.ts
+export async function getImageUrl(id: number | null): Promise<string | null> {
+  if (!id) return null;
+  const res = await fetch(`https://cms.maxilodentalstudio.com/wp-json/wp/v2/media/${id}`);
+  const data = await res.json();
+  return data.source_url;
 }
